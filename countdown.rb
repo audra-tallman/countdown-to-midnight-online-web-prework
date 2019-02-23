57040b1d1090c09ef26d0c59fc48c0991dbd64d6
Audra Tallman <audra.tallman@hotmail.com>
2019-02-23 01:54:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,18 @@
 #write your code here
-
+n = 1
 def countdown
-  while number > 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
+  while n > 0
+  puts "#{n} SECOND(S)!"
+  n -= 1
   end 
   puts "HAPPY NEW YEAR"
   end
 end
 
 def countdown_with_sleep
-  while number > 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
+  while n > 0
+  puts "#{n} SECOND(S)!"
+  n -= 1
   sleep 1
   end
   puts "HAPPY NEW YEAR"
