13a7dbf0b51b1f1e9f9ee4b574fd8ad8c226e642
Audra Tallman <audra.tallman@hotmail.com>
2019-02-23 01:57:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 #write your code here
 n = 10
-def countdown
+def countdown(n)
   while n > 0
   puts "#{n} SECOND(S)!"
   n -= 1
@@ -8,11 +8,11 @@ def countdown
   puts "HAPPY NEW YEAR"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(n)
   while n > 0
   puts "#{n} SECOND(S)!"
   n -= 1
-  sleep 1
+  sleep 5
   end
   puts "HAPPY NEW YEAR"
 end 
