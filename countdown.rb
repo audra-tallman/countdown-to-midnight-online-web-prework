c394b245d69bd031791151ac610ffa7736c8bba4
Audra Tallman <audra.tallman@hotmail.com>
2019-02-23 01:52:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,17 @@ def countdown
   while number > 0
   puts "#{number} SECOND(S)!"
   number -= 1
-  
+  end 
   puts "HAPPY NEW YEAR"
+  end
 end
+
+def countdown_with_sleep
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep 1
+  puts "HAPPY NEW YEAR"
+  end
+  
+  
